cb7c5aeef9652445fa058a3600e55fd39523b4af
Elliott Pogue <epogue@Mercury.local>
2010-06-09 23:24:17 -0400

    Ignoring iFrames

--- Laterstars.safariextension/laterstars.js
+++ Laterstars.safariextension/laterstars.js
@@ -1,35 +1,40 @@
-function handleMessage(msgEvent) {
-	var messageName = msgEvent.name;
-	var messageData = msgEvent.message;
-	if (messageName === "laterstars-faved") {		
-		var d = document,
-				s = d.createElement('script'),
-				c = d.createElement('link'),
-				b = d.body;
-		
-		try {
-			if (!b) throw (0);
-			
-			// Insert CSS
-			c.setAttribute('href', 'http://laterstars.com/stylesheets/oneclick.css');
-			c.setAttribute('media', 'all');
-			c.setAttribute('rel', 'stylesheet');
-			c.setAttribute('type', 'text/css');
-			
-			// Insert message div
-			var div = d.createElement('div');
-			div.setAttribute('id','lsbm-bezel');
-			div.innerHTML = '&#x2605;&nbsp;&nbsp;faving for later...';
-			b.appendChild(c);
-			b.appendChild(div);
-			
-			// Insert script
-			s.setAttribute('src', 'http://laterstars.com/epogue/bookmarklet/oneclick?url=' + encodeURIComponent(d.location.href) + '&title=' + encodeURIComponent(d.title));
-			b.appendChild(s);
-		} catch(e) {
-			alert('simma');
+var isInIFrame = (window.location != window.parent.location) ? true : false;
+if (isInIFrame == true) {
+	
+} else {
+	function handleMessage(msgEvent) {
+		var messageName = msgEvent.name;
+		var messageData = msgEvent.message;
+		if (messageName === "laterstars-faved") {		
+			var d = document,
+					s = d.createElement('script'),
+					c = d.createElement('link'),
+					b = d.body;
+
+			try {
+				if (!b) throw (0);
+
+				// Insert CSS
+				c.setAttribute('href', 'http://laterstars.com/stylesheets/oneclick.css');
+				c.setAttribute('media', 'all');
+				c.setAttribute('rel', 'stylesheet');
+				c.setAttribute('type', 'text/css');
+
+				// Insert message div
+				var div = d.createElement('div');
+				div.setAttribute('id','lsbm-bezel');
+				div.innerHTML = '&#x2605;&nbsp;&nbsp;faving for later...';
+				b.appendChild(c);
+				b.appendChild(div);
+
+				// Insert script
+				s.setAttribute('src', 'http://laterstars.com/epogue/bookmarklet/oneclick?url=' + encodeURIComponent(d.location.href) + '&title=' + encodeURIComponent(d.title));
+				b.appendChild(s);
+			} catch(e) {
+				alert('simma');
+			}
 		}
 	}
-}
 
-safari.self.addEventListener("message", handleMessage, false);
+	safari.self.addEventListener("message", handleMessage, false);
+}
