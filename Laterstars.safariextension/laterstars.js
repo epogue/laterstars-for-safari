0559872ae3f13e67ac0e9598956228405165f031
Elliott Pogue <poguere@me.com>
2010-06-11 23:59:07 -0400

    Adding a way to choose between opening Laterstars or One-click Faving by default. The shift key will do the opposite effect of the default.

--- Laterstars.safariextension/laterstars.js
+++ Laterstars.safariextension/laterstars.js
@@ -1,41 +1,64 @@
 var isInIFrame = (window.location != window.parent.location) ? true : false;
 if (isInIFrame == true) {
-	
 } else {
+	var shift = false;
 	
 	function handleMessage(msgEvent) {
 		var messageName = msgEvent.name;
 		var messageData = msgEvent.message;
 		if (messageName === "laterstars-faved") {		
-			var d = document,
-					s = d.createElement('script'),
-					c = d.createElement('link'),
-					b = d.body;
-
-			try {
-				if (!b) throw (0);
-
-				// Insert CSS
-				c.setAttribute('href', 'http://laterstars.com/stylesheets/oneclick.css');
-				c.setAttribute('media', 'all');
-				c.setAttribute('rel', 'stylesheet');
-				c.setAttribute('type', 'text/css');
-
-				// Insert message div
-				var div = d.createElement('div');
-				div.setAttribute('id','lsbm-bezel');
-				div.innerHTML = '&#x2605;&nbsp;&nbsp;faving for later...';
-				b.appendChild(c);
-				b.appendChild(div);
-
-				// Insert script
-				s.setAttribute('src', 'http://laterstars.com/' + encodeURI(messageData.twitterUsername) + '/bookmarklet/oneclick?url=' + encodeURIComponent(d.location.href) + '&title=' + encodeURIComponent(d.title));
-				b.appendChild(s);
-			} catch(e) {
-				alert('simma');
+			if ((shift == 1 && messageData.buttonBehavior == "open") || (shift == 0 && messageData.buttonBehavior == "oneClick")) {
+				oneClick();
+			} else {
+				safari.self.tab.dispatchMessage("openLaterstars","");
+				shift = 0;
 			}
+		}
+	}
+	
+	function oneClick(){
+		var d = document,
+				s = d.createElement('script'),
+				c = d.createElement('link'),
+				b = d.body;
+
+		try {
+			if (!b) throw (0);
+
+			// Insert CSS
+			c.setAttribute('href', 'http://laterstars.com/stylesheets/oneclick.css');
+			c.setAttribute('media', 'all');
+			c.setAttribute('rel', 'stylesheet');
+			c.setAttribute('type', 'text/css');
+
+			// Insert message div
+			var div = d.createElement('div');
+			div.setAttribute('id','lsbm-bezel');
+			div.innerHTML = '&#x2605;&nbsp;&nbsp;faving for later...';
+			b.appendChild(c);
+			b.appendChild(div);
+
+			// Insert script
+			s.setAttribute('src', 'http://laterstars.com/' + encodeURI(messageData.twitterUsername) + '/bookmarklet/oneclick?url=' + encodeURIComponent(d.location.href) + '&title=' + encodeURIComponent(d.title));
+			b.appendChild(s);
+		} catch(e) {
+			alert('simma');
+		}
+	}
+	
+	function handleKeyDown(event){
+		if (event.keyCode == '16') {
+			shift = 1;
+		}
+	}
+	
+	function handleKeyUp(event){
+		if (event.keyCode == '16') {
+			shift = 0;
 		}
 	}
 
 	safari.self.addEventListener("message", handleMessage, false);
+	document.addEventListener("keydown", handleKeyDown, false);
+	document.addEventListener("keyup", handleKeyUp, false);
 }
